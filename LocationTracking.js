4a50be23156954c2f064c24888ac7de0f20029f3
NovelHarmony79 <launch112020@gmail.com>
2024-08-09 08:57:36 -0700

    Update LocationTracking.js

--- LocationTracking.js
+++ LocationTracking.js
@@ -4,24 +4,43 @@ import axios from 'axios';
 
 const LOCATION_TASK_NAME = 'background-location-task';
 
+let locationCallback = null;
+
 TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
   if (error) {
-    console.error(error);
+    console.error("TaskManager error:", error);
     return;
   }
   if (data) {
     const { locations } = data;
     const location = locations[0];
+    const coords = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+    if (locationCallback) {
+      locationCallback(coords);
+    }
     try {
-      await axios.post('http://your-raspberry-pi-ip:5000/location', {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
+      await axios.post('http://your-raspberry-pi-ip:5000/location', coords);
     } catch (err) {
-      console.error(err);
+      console.error("Error sending location to server:", err);
     }
   }
 });
+
+export const startLocationUpdates = async (callback) => {
+  locationCallback = callback;
+  const { status } = await Location.requestBackgroundPermissionsAsync();
+  if (status === 'granted') {
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      accuracy: Location.Accuracy.High,
+      distanceInterval: 10, // Update every 10 meters
+    });
+  } else {
+    console.error('Location permission not granted');
+  }
+};
 
 export const startLocationUpdates = async () => {
   const { status } = await Location.requestBackgroundPermissionsAsync();
@@ -30,6 +49,11 @@ export const startLocationUpdates = async () => {
       accuracy: Location.Accuracy.High,
       distanceInterval: 10, // Update every 10 meters
     });
+  } else {
+    console.error('Location permission not granted');
+  }
+};
+
   } else {
     console.error('Location permission not granted');
   }
